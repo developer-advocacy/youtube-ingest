fc1574f73c2c2a0377a235d30802664068f8e522
Josh Long <josh@joshlong.com>
2022-07-05 16:37:39 -0700

    videos

--- src/main/java/com/joshlong/youtube/YoutubeIngestApplication.java
+++ src/main/java/com/joshlong/youtube/YoutubeIngestApplication.java
@@ -1,6 +1,5 @@
 package com.joshlong.youtube;
 
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
