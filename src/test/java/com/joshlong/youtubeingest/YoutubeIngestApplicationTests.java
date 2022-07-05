6d9a5bcea3e54567f7b8672c2a154badeba3f153
Josh Long <josh@joshlong.com>
2022-07-05 14:16:40 -0700

    youtube

---
+++ src/test/java/com/joshlong/youtubeingest/YoutubeIngestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joshlong.youtubeingest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeIngestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
