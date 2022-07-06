2b05fd04eed3269b2bada26e73ae4b0454c3173b
Josh Long <josh@joshlong.com>
2022-07-05 18:29:27 -0700

    videos

--- src/test/java/com/joshlong/youtube/client/DefaultYoutubeClientTest.java
+++ src/test/java/com/joshlong/youtube/client/DefaultYoutubeClientTest.java
@@ -5,7 +5,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.util.Assert;
+
+import java.util.List;
 
 @Slf4j
 @SpringBootTest
@@ -30,16 +31,22 @@ class DefaultYoutubeClientTest {
 	}
 
 	@Test
+	void videosById() throws Exception {
+		var videosMap = this.youtubeClient.getVideosByIds(List.of("Ks-_Mh1QhMc", "Cc0KYU2j0TM4", "CeIho2S0ZahI"));
+		Assertions.assertEquals(videosMap.size(), 3);
+	}
+
+	@Test
 	void videoById() throws Exception {
 		var video = this.youtubeClient.getVideoById("eIho2S0ZahI");
-		Assert.isTrue(video.videoId().equalsIgnoreCase("eIho2S0ZahI"), "you don't have a valid video ID");
-		Assert.isTrue(!video.tags().isEmpty(), "there should be more than zero tags");
-		Assert.isTrue(video.tags().contains("Julian Treasure"), "the tags should include the speaker");
-		Assert.isTrue(video.viewCount() >= 13852996, "the view count should line up with what's on YT'");
-		Assert.isTrue(video.commentCount() >= 9596, "the comment count should line up with what's on YT'");
-		Assert.isTrue(video.likeCount() >= 354551, "the like count should line up with what's on YT'");
-		Assert.isTrue(video.favoriteCount() >= 0, "the favorite count should line up with what's on YT'");
-		Assert.isTrue(video.title().contains("How to speak so that people want to listen"),
+		Assertions.assertTrue(video.videoId().equalsIgnoreCase("eIho2S0ZahI"), "you don't have a valid video ID");
+		Assertions.assertTrue(!video.tags().isEmpty(), "there should be more than zero tags");
+		Assertions.assertTrue(video.tags().contains("Julian Treasure"), "the tags should include the speaker");
+		Assertions.assertTrue(video.viewCount() >= 13852996, "the view count should line up with what's on YT'");
+		Assertions.assertTrue(video.commentCount() >= 9596, "the comment count should line up with what's on YT'");
+		Assertions.assertTrue(video.likeCount() >= 354551, "the like count should line up with what's on YT'");
+		Assertions.assertTrue(video.favoriteCount() >= 0, "the favorite count should line up with what's on YT'");
+		Assertions.assertTrue(video.title().contains("How to speak so that people want to listen"),
 				"the title must reflect what's shown on YT");
 	}
 
